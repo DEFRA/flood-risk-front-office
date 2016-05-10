6b3225c2f74f0c3b8a56ffc6bd94f40744dc6248
Rob Nichols <robnichols.ea@gmail.com>
2016-05-10 14:09:46 +0100

    Update session store initializer to use active_record_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_state_engine_test_session'
+Rails.application.config.session_store :active_record_store, key: "flood-risk-front-office"
