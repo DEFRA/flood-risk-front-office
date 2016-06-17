60e7db030687a18805217d44e5c0761f94e04541
Tim Crowe <tim@woodpigeon.com>
2016-06-17 19:00:31 +0100

    Remove obsolete contact_ea_location page and Check Location relics
    * Remove obsolete contact_ea_location page (this page also exists
    in flood_risk_engine but another PR removed it from there too)
    * Remove obsolete flood_risk_engine configuration settings
    * Add a missing css .contact class for the new contact ea page
    when it arrives

--- config/initializers/flood_risk_engine.rb
+++ config/initializers/flood_risk_engine.rb
@@ -1,5 +1,1 @@
-FloodRiskEngine.configure do |config|
-  config.redirection_url_on_location_unchecked = "/pages/contact_ea_location"
-end
-
 FloodRiskEngine::ApplicationController.layout 'application'
