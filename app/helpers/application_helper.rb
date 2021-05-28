9d152c12082daf1303b0fe40f44b1f3972d0680d
Toby Privett <toby@snaplab.co.uk>
2021-05-28 14:50:10 +0100

    PECR Audit - change feedback link
    https://eaflood.atlassian.net/browse/RUBY-1465

--- app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-module ApplicationHelper
-
-  def self.feedback_survey_url(current_title)
-    survey_url = "https://www.smartsurvey.co.uk/s/floodexemptions/?"
-    survey_params = { referringpage: current_title }
-    survey_url + survey_params.to_query
-  end
-
-end
