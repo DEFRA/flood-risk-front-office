4ad92477d32abb8315d558437f7bc423a9074988
Toby Privett <toby@snaplab.co.uk>
2021-07-08 14:50:28 +0100

    Make cookie consent banner functional
    https://eaflood.atlassian.net/browse/RUBY-1488
    
    This commit toggles the Google Tag Manager state
    
    - If a user has the cookie: cookies_policy => :analytics_accepted,
    the google analytics tag is rendered with the GOOGLE_ANALYTICS_ID
    
    - Otherwise the google analytics tag is rendered with 'ga-disable-'
    appended to the GOOGLE_ANALYTICS_ID

--- spec/features/cookies_spec.rb
+++ spec/features/cookies_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.feature "Cookies", type: :feature do
   let(:cookie_banner_div) { ".govuk-cookie-banner" }
 
+  before { ENV["GOOGLE_ANALYTICS_ID"] = "GA_ID" }
+
   scenario "User accepts analytics cookies" do
     visit "/"
     expect(page).to have_link("View cookies", href: "/pages/cookies")
@@ -16,11 +18,20 @@ RSpec.feature "Cookies", type: :feature do
     end
 
     expect(page).not_to have_css(cookie_banner_div)
+
+    # Note that the google analytics tag is only parsed into a URL
+    # if javascript is enabled.
+    # Here we check that the GOOGLE_ANALYTICS_ID is rendered as expected
+    expect(page.source).to have_text("'script','dataLayer','GA_ID'")
   end
 
   scenario "User rejects analytics cookies" do
     visit "/"
     click_on "Reject analytics cookies"
     expect(page).to have_text("You’ve rejected analytics cookies")
+
+    click_on "Hide this message"
+
+    expect(page.source).to have_text("'script','dataLayer','ga-disable-GA_ID'")
   end
 end
