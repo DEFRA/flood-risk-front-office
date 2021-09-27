d99825ad60c96bc152bb2a71c837fb45a510b08e
Toby Privett <toby@snaplab.co.uk>
2021-09-27 13:32:06 +0100

    Upgrade to Rails 6.1 (#375)
    https://eaflood.atlassian.net/browse/RUBY-1605

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,9 +37,6 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
-  # Raises error for missing translations
-  config.action_view.raise_on_missing_translations = true
-
   # Resolves issue with sass-rails
   # https://github.com/sass/sassc-rails/issues/93
   config.assets.css_compressor = nil
