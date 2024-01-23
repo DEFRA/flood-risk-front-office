7a7cc66c40d8853d29435ad50670237608403b6b
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-01-23 19:21:38 +0000

    Replace Rails .secrets functionality with application-specific version

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module FloodRiskFrontOffice
   class Application < Rails::Application
+    config.load_defaults 7.0
+
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     config.to_prepare do
@@ -37,7 +39,14 @@ module FloodRiskFrontOffice
     # https://github.com/alphagov/govuk-frontend/issues/1350
     config.assets.css_compressor = nil
 
-    # For Rails 7: https://guides.rubyonrails.org/active_record_multiple_databases.html#migrate-to-the-new-connection-handling
-    config.active_record.legacy_connection_handling = false
+    # Rails secrets is deprecated in favour of Rails credentials, but that
+    # doesn't work for us so we want to mimic Rails secrets functionality:
+    def self.secrets
+      @secrets ||= begin
+        secrets = ActiveSupport::OrderedOptions.new
+        files = config.paths["config/secrets"].existent
+        secrets.merge! Rails::Secrets.parse(files, env: Rails.env)
+      end
+    end
   end
 end
